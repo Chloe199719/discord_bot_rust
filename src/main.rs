12f399187ca904b7e6b596763c2db42fcf6ad168
Chloe Pratas <chloevision97@gmail.com>
2024-03-31 23:33:25 +0200

    Add car_state command to retrieve car state information

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use chrono::format;
+use chrono::{format, DateTime};
 use poise::serenity_prelude as serenity;
 
 use ::serenity::{all::ChannelType, builder::CreateChannel};
@@ -13,6 +13,9 @@ type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
 use serenity::model::gateway::GatewayIntents;
+
+use crate::car_types::Car;
+mod car_types;
 // #[group]
 // #[commands(ping, am_i_admin, am_i_cool, random_quote, test)]
 // struct General;
@@ -200,11 +203,143 @@ async fn battery(ctx: Context<'_>) -> Result<(), Error> {
         .await?;
     let car_data: CarBattery = car_response.json().await?;
 
-    let carResponse = ctx
-        .say(format!("Tessie Battery is {}", car_data.battery_level))
+    _ = ctx
+        .say(format!(
+            "Jessie Battery is {}% at {}",
+            car_data.battery_level,
+            DateTime::from_timestamp(car_data.timestamp, Default::default())
+                .unwrap()
+                .format("%Y-%m-%d %H:%M:%S")
+                .to_string()
+        ))
         .await?;
     Ok(())
 }
+
+#[poise::command(slash_command, prefix_command)]
+async fn car_state(ctx: Context<'_>) -> Result<(), Error> {
+    let client = reqwest::Client::new();
+    let car_response = client
+        .get("https://api.tessie.com/LRW3E7FS6RC040530/state?use_cache=true")
+        .header(
+            "Authorization",
+            format!("Bearer {}", env::var("TESSIE_KEY").unwrap()),
+        )
+        .send()
+        .await?;
+    let car_data: Car = car_response.json().await?;
+
+    _ = ctx
+        .say(format!(
+            "{} Battery is {}% at {}",
+            car_data.display_name,
+            car_data.charge_state.battery_level,
+            DateTime::from_timestamp_millis(car_data.charge_state.timestamp)
+                .unwrap()
+                .format("%H:%M:%S %d-%m-%Y")
+                .to_string()
+        ))
+        .await?;
+    ctx.say(format!(
+        "{} is in {} state",
+        car_data.display_name, car_data.state
+    ))
+    .await?;
+    ctx.say(format!(
+        "Charge State:
+        Battery Heater On: {}
+        Battery Level: {}
+        Battery Range: {}
+        Charge Amps: {}
+        Charge Current Request: {}
+        Charge Current Request Max: {}
+        Charge Enable Request: {}
+        Charge Energy Added: {}
+        Charge Limit SOC: {}
+        Charge Limit SOC Max: {}
+        Charge Limit SOC Min: {}
+        Charge Limit SOC Std: {}
+        Charge Miles Added Ideal: {}
+        Charge Miles Added Rated: {}
+        Charge Port Cold Weather Mode: {}
+        Charge Port Color: {}
+        Charge Port Door Open: {}
+        Charge Port Latch: {}
+        Charge Rate: {}
+        Charger Actual Current: {}
+        Charger Phases: {}
+        Charger Pilot Current: {}
+        Charger Power: {}
+        Charger Voltage: {}
+        Charging State: {}
+        Estimated Battery Range: {}
+        Fast Charger Present: {}
+        Fast Charger Type: {}
+        Ideal Battery Range: {}
+        Managed Charging Active: {}
+        Minutes To Full Charge: {}
+        Not Enough Power To Heat: {}
+        Scheduled Charging Pending: {}
+        Scheduled Charging Start Time: {}
+        Scheduled Charging Start Time App: {}
+        Scheduled Departure Time: {}
+        Scheduled Departure Time Minutes: {}
+        Supercharger Session Trip Planner: {}
+        Time To Full Charge: {}
+        Usable Battery Level: {}
+        ",
+        car_data.charge_state.battery_heater_on,
+        car_data.charge_state.battery_level,
+        car_data.charge_state.battery_range,
+        car_data.charge_state.charge_amps,
+        car_data.charge_state.charge_current_request,
+        car_data.charge_state.charge_current_request_max,
+        car_data.charge_state.charge_enable_request,
+        car_data.charge_state.charge_energy_added,
+        car_data.charge_state.charge_limit_soc,
+        car_data.charge_state.charge_limit_soc_max,
+        car_data.charge_state.charge_limit_soc_min,
+        car_data.charge_state.charge_limit_soc_std,
+        car_data.charge_state.charge_miles_added_ideal,
+        car_data.charge_state.charge_miles_added_rated,
+        car_data.charge_state.charge_port_cold_weather_mode,
+        car_data.charge_state.charge_port_color,
+        car_data.charge_state.charge_port_door_open,
+        car_data.charge_state.charge_port_latch,
+        car_data.charge_state.charge_rate,
+        car_data.charge_state.charger_actual_current,
+        car_data.charge_state.charger_phases.unwrap_or(0),
+        car_data.charge_state.charger_pilot_current,
+        car_data.charge_state.charger_power,
+        car_data.charge_state.charger_voltage,
+        car_data.charge_state.charging_state,
+        car_data.charge_state.est_battery_range,
+        car_data.charge_state.fast_charger_present,
+        car_data.charge_state.fast_charger_type,
+        car_data.charge_state.ideal_battery_range,
+        car_data.charge_state.off_peak_charging_enabled,
+        car_data.charge_state.minutes_to_full_charge,
+        car_data
+            .charge_state
+            .not_enough_power_to_heat
+            .as_ref()
+            .unwrap_or(&"None".to_string()),
+        car_data.charge_state.scheduled_charging_pending,
+        car_data
+            .charge_state
+            .scheduled_charging_start_time
+            .unwrap_or(0),
+        car_data.charge_state.scheduled_charging_start_time_app,
+        car_data.charge_state.scheduled_departure_time,
+        car_data.charge_state.scheduled_departure_time_minutes,
+        car_data.charge_state.supercharger_session_trip_planner,
+        car_data.charge_state.time_to_full_charge,
+        car_data.charge_state.usable_battery_level,
+    ))
+    .await?;
+    Ok(())
+}
+
 #[derive(Deserialize, Debug, Clone)]
 struct CarBattery {
     battery_level: u32,
@@ -239,6 +374,7 @@ async fn main() {
                 cr_di(),
                 cr_parent(),
                 battery(),
+                car_state(),
             ],
             prefix_options: poise::PrefixFrameworkOptions {
                 prefix: Some("!".to_string()),
